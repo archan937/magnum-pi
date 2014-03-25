97aefafbb6970a8ccc62c5a231873e2413a5b08e
Paul Engel <pm_engel@icloud.com>
2014-03-25 18:36:21 +0100

    Being able to extend a class with MagnumPI which actually extends the class with MagnumPI::API

--- lib/magnum-pi.rb
+++ lib/magnum-pi.rb
@@ -4,3 +4,9 @@ require "oj"
 require "magnum-pi/dsl"
 require "magnum-pi/api"
 require "magnum-pi/version"
+
+module MagnumPI
+  def self.extended(base)
+    base.extend API
+  end
+end
--- test/unit/test_magnum-pi.rb
+++ test/unit/test_magnum-pi.rb
@@ -3,12 +3,25 @@ require_relative "../test_helper"
 module Unit
   class TestMagnumPI < MiniTest::Test
 
+    class SomeAPI
+      extend MagnumPI
+    end
+
     describe MagnumPI do
       it "has the current version" do
         version = "0.1.0"
         assert_equal version, MagnumPI::VERSION
         assert File.read(path "CHANGELOG.rdoc").include?("Version #{version}")
         assert File.read(path "VERSION").include?(version)
+      end
+
+      describe "when extended" do
+        it "also extends base with MagnumPI::API::Consumer" do
+          def SomeAPI.extended_modules
+            (class << self; self end).included_modules
+          end
+          assert_equal true, SomeAPI.extended_modules.include?(MagnumPI::API)
+        end
       end
     end
 
