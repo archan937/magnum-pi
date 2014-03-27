caab27c246777bf4da46959c421aa8b5eb75f0ca
Paul Engel <pm_engel@icloud.com>
2014-03-27 01:19:38 +0100

    Tackling arrays within Mechanize::Util.build_query_string

--- lib/magnum-pi/gem_ext/mechanize/util.rb
+++ lib/magnum-pi/gem_ext/mechanize/util.rb
@@ -2,6 +2,7 @@ class Mechanize
   class Util
 
     def self.build_query_string(parameters, enc = nil)
+      parameters = Hash[parameters] unless parameters.is_a?(Hash)
       parameters.to_query
     end
 
